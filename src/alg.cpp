eece8fdf9f4de6f51de2d726eff10548e4c3d098
AnastasiaSoldatkina <94635746+AnastasiaSoldatkina@users.noreply.github.com>
2022-03-01 18:43:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int y = 0;
+    for (int i = 0; size > i; i++) {
+        if (arr[i] == value) {
+            y++;
+        }
+    }
+    return y;
 }
