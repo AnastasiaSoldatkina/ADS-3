13434fdf1ec7f26cff875d9b86433dfe6cfd9ff3
AnastasiaSoldatkina <94635746+AnastasiaSoldatkina@users.noreply.github.com>
2022-03-11 20:42:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,35 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int y = 0;
-    for (int i = 0; size > i; i++) {
-        if (arr[i] == value) {
-            y++;
+ int i = 0;
+    int j = size - 1; 
+    int d = 0;
+    while (i < j) {
+        int x = i + (j - i) / 2;
+        if (i == j - 1) {
+            return 0;
+            break;
+        }
+        d = x;
+        if (arr[x] == value) {
+            while (arr[x] == value) {
+                x++;
+            }
+            j = x;
+            x = d;
+            while (arr[x] == value) {
+                x--;
+            }
+            i = x;
+            return j - i - 1;
+            break;
+        }  
+        if (arr[x] < value) {
+            i = x;
+            x++;
+        } else {
+            j = x - 1;
+            x--;
         }
     }
-    return y;
 }
